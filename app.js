052d698909e9ca43409651ce0b50cf4d97696628
liforra <git@liforra.de>
2025-08-06 09:33:10 +0200

    Added .env file and ENV Vars for Port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT;
 
 // Middleware for static files
 app.use(express.static('public'));
